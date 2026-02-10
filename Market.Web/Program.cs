18a89bb625d0314a37db7928180b93c52b6e8a74
Kacper Kotecki <kacperkotecki@o2.pl>
2026-02-10 11:20:52 +0100

    Update navigation and routing to use Auctions controller Auctions/Index

--- Market.Web/Program.cs
+++ Market.Web/Program.cs
@@ -73,7 +73,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Auctions}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
